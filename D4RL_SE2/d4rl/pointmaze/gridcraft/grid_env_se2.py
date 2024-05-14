953d986efe95d9414ec382293cb51d0f091da7ca
Tony Tong <ztangaj@connect.ust.hk>
2024-05-15 01:05:58 +0800

    fix se2 env

--- D4RL_SE2/d4rl/pointmaze/gridcraft/grid_env_se2.py
+++ D4RL_SE2/d4rl/pointmaze/gridcraft/grid_env_se2.py
@@ -3,9 +3,12 @@ import numpy as np
 import gym
 import gym.spaces
 import pygame
+from d4rl.offline_env import OfflineEnv
+import h5py
+from tqdm import tqdm
 
 from D4RL_SE2.d4rl.pointmaze.gridcraft import grid_spec_se2
-from D4RL_SE2.d4rl.pointmaze.gridcraft.grid_spec_se2 import EMPTY, WALL, TILES, START, RENDER_DICT, REWARD
+from D4RL_SE2.d4rl.pointmaze.gridcraft.grid_spec_se2 import EMPTY, WALL, TILES, START, RENDER_DICT, REWARD, GOAL
 
 ACT_NOOP = 0
 ACT_UP = 1
@@ -16,12 +19,12 @@ ACT_CW = 5
 ACT_CCW = 6
 ACT_DICT = {
     ACT_NOOP: [0, 0, 0],
-    ACT_UP: [0, -1, 0],
-    ACT_LEFT: [-1, 0, 0],
-    ACT_RIGHT: [+1, 0, 0],
-    ACT_DOWN: [0, +1, 0],
-    ACT_CW: [0, 0, +1], # 1 means rotating for THETA_GRID_RES rad
-    ACT_CCW: [0, 0, -1]
+    ACT_UP: [0, -0.1, 0],
+    ACT_LEFT: [-0.1, 0, 0],
+    ACT_RIGHT: [+0.1, 0, 0],
+    ACT_DOWN: [0, +0.1, 0],
+    ACT_CW: [0, 0, +0.1], # 1 means rotating for THETA_GRID_RES rad
+    ACT_CCW: [0, 0, -0.1]
 }
 ACT_TO_STR = {
     ACT_NOOP: 'NOOP',
@@ -34,6 +37,17 @@ ACT_TO_STR = {
 }
 
 SEGMENT_LENGTH = 1.5
+
+
+def get_keys(h5file):
+    keys = []
+
+    def visitor(name, item):
+        if isinstance(item, h5py.Dataset):
+            keys.append(name)
+
+    h5file.visititems(visitor)
+    return keys
 
 class TransitionModel(object):
     def __init__(self, gridspec, eps=0.2):
@@ -94,17 +108,30 @@ class RewardFunction(object):
         return self.default
 
 
+O_MAZE = \
+        "########\\"+\
+        "#OOOOOO#\\"+\
+        "#OOOOOO#\\"+\
+        "#OO##OO#\\"+\
+        "#OO##OO#\\"+\
+        "#OOOOOO#\\"+\
+        "#OOOOOO#\\"+\
+        "########"
+
+# class GridEnvSE2(OfflineEnv, gym.Env):
 class GridEnvSE2(gym.Env):
-    def __init__(self, maze_str,
+    def __init__(self, maze_str=O_MAZE,
                  tiles=TILES,
                  rew_fn=None,
                  teps=0.0, 
                  max_timesteps=None,
                  rew_map=None,
                  terminal_states=None,
+                 dataset_path=None,
                  default_rew=0):
         self._env_args = {'teps': teps, 'max_timesteps': max_timesteps}
         self.str_maze_spec = maze_str
+        self.dataset_path = dataset_path
         print('maze_str:', maze_str)
         #type
         print('type(maze_str):', type(maze_str))
@@ -139,6 +166,17 @@ class GridEnvSE2(gym.Env):
         self.render_mode = "human"
         self.render_fps = 30
         self.size = np.maximum(self.gs.width, self.gs.height)
+        
+        self.goal_locations = list(zip(*np.where(self.maze_arr == GOAL)))
+        if len(self.goal_locations) == 1:
+            self.set_target(self.goal_locations[0])
+        elif len(self.goal_locations) > 1:
+            raise ValueError("More than 1 goal specified!")
+        else:
+            # If no goal, use the first empty tile
+            # self.set_target(np.array(self.reset_locations[0]).astype(self.observation_space.dtype))
+            self.reset_model()
+            self.set_target()
         super(GridEnvSE2, self).__init__()
 
 
@@ -367,10 +405,50 @@ class GridEnvSE2(gym.Env):
             pygame.display.quit()
             pygame.quit()
 
+    # def get_dataset(self, h5path=None):
+    #     if h5path is None:
+    #         h5path = self.dataset_path
+    #     return super(GridEnvSE2, self).get_dataset(h5path)
+
+    def get_dataset(self, h5path=None):
+        if h5path is None:
+            h5path = self.dataset_path
+        data_dict = {}
+        print(f'^&^& load dataset from {h5path}')
+        with h5py.File(h5path, 'r') as dataset_file:
+            for k in tqdm(get_keys(dataset_file), desc="load datafile"):
+                try:  # first try loading as an array
+                    data_dict[k] = dataset_file[k][:]
+                    # print(f'^&^& dataset shape: {data_dict[k].shape}')
+                except ValueError as e:  # try loading as a scalar
+                    data_dict[k] = dataset_file[k][()]
+        # return data_dict
+        
+        # Run a few quick sanity checks
+        for key in ['observations', 'actions', 'rewards', 'terminals']:
+            assert key in data_dict, 'Dataset is missing key %s' % key
+        N_samples = data_dict['observations'].shape[0]
+        if self.observation_space.shape is not None:
+            assert data_dict['observations'].shape[1:] == self.observation_space.shape, \
+                'Observation shape does not match env: %s vs %s' % (
+                    str(data_dict['observations'].shape[1:]), str(self.observation_space.shape))
+        assert data_dict['actions'].shape[1:] == self.action_space.shape, \
+            'Action shape does not match env: %s vs %s' % (
+                str(data_dict['actions'].shape[1:]), str(self.action_space.shape))
+        if data_dict['rewards'].shape == (N_samples, 1):
+            data_dict['rewards'] = data_dict['rewards'][:, 0]
+        assert data_dict['rewards'].shape == (N_samples,), 'Reward has wrong shape: %s' % (
+            str(data_dict['rewards'].shape))
+        if data_dict['terminals'].shape == (N_samples, 1):
+            data_dict['terminals'] = data_dict['terminals'][:, 0]
+        assert data_dict['terminals'].shape == (N_samples,), 'Terminals has wrong shape: %s' % (
+            str(data_dict['rewards'].shape))
+        return data_dict
+
     # TODO: DONE
     def set_target(self, target_location=None):
         if target_location is None:
-            feasible_target = True;
+            feasible_target = True
 
             for _ in range(100):
                 feasible_target = True
@@ -404,13 +482,18 @@ class GridEnvSE2(gym.Env):
 
     @property
     def action_space(self):
-        return gym.spaces.Discrete(5)
+        # space = gym.spaces.Discrete(7)
+        space = gym.spaces.Box(0, 1, shape=(3,))
+        return space
 
     @property
     def observation_space(self):
+        print('execute observation_space')
         dO = len(self.gs)
-        #return gym.spaces.Box(0,1,shape=dO)
-        return gym.spaces.Discrete(dO)
+        print(dO)
+        return gym.spaces.Box(0,1,shape=(6,))
+        # return gym.spaces.Box(0,1,shape=dO)
+        # return gym.spaces.Discrete(dO)
 
     # TODO: DONE
     def get_transitions(self, s, a):
